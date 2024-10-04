86030ba3d8b31dd2967d80e55b6ce89a0f0f38d6
Rishat Imangulov <imangulov_r@mail.ru>
2024-10-05 02:01:27 +0500

    Initial + sql create

---
+++ src/main/java/org/richard/backend/CompServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package org.richard.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompServiceBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/db/changelog/v0.0.1/changeset-0.0.0-0-create-schemas.sql
@@ -0,0 +1,2 @@
+CREATE SCHEMA if not exists ticketing_schema;
+CREATE SCHEMA if not exists product_schema;
